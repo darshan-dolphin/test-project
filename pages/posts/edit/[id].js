9d39ee07cecf161a2d54fed55628eb7f68ef6ac8
darshan-dwspl <darshan.dolphinwebsolutions@gmail.com>
2022-02-16 11:51:49 +0530

    16-feb-3

--- pages/posts/edit/[id].js
+++ pages/posts/edit/[id].js
@@ -4,23 +4,7 @@ import Link from "next/link";
 import { useEffect } from "react";
 import Layout from "../../../components/layout";
 
-export async function getStaticPaths() {
-    // const res = await fetch('/api/ninja/users')
-    // const data = await res.json()
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = await response.json()
-
-    const paths = data.map(ninja => {
-        return {
-            params: { id: ninja.id.toString() }
-        }
-    })
-
-    // Return a list of possible value for id
-    return { paths, fallback: false }
-}
-
-export async function getStaticProps(context) {
+export async function getServerSideProps(context) {
     const id = context.params.id
     // const res = await fetch('/api/ninja/user', {
     //     method: "POST",
